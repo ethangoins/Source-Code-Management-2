2185194227b9cb5afda3bb588949ca44a77c9146
goins65504 <goins65504@mvctc.com>
2024-03-26 10:23:04 -0400

    Static casted average function so it does the math correct. Changed the second n2 to n3 to fix math. Added exit sequence.

--- BadMath/Source.cpp
+++ BadMath/Source.cpp
@@ -4,7 +4,8 @@ using namespace std;
 
 float average(int i1, int i2, int i3)
 {
-	return i1 + i2 + i3 / 3.0f;
+	float average = (static_cast<float>(i1 + i2 + i3) / 3);						
+	return average;
 }
 
 int main()
@@ -13,11 +14,14 @@ int main()
 	cout << "This program calculates the average of three numbers." << endl;
 	cout << "First number: "; cin >> n1;
 	cout << "Second number: "; cin >> n2;
-	cout << "Third number: "; cin >> n2;
+	cout << "Third number: "; cin >> n3;
 
 	float a = average(n1, n2, n3);
 
 	cout << setprecision(1) << fixed << "The average is " << a << endl;
 
+	cout << "Press enter to exit program";
+	cin.ignore();
+	cin.get();
 	return 0;
 }
